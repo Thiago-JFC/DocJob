0b9d4df8b164a8750fa4e1517b357fba505ce3fc
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2025-04-28 15:00:05 -0300

    configuring DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RouterModule } from '@nestjs/core';
 import { DocumentModule } from './routes/document/document.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,7 +15,20 @@ import { DocumentModule } from './routes/document/document.module';
         path: 'document',
         module: DocumentModule
       }
-    ])
+    ]),
+
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,  
+      database: process.env.DB_NAME,
+      synchronize: true,
+    }),
+
   ],
   controllers: [AppController],
   providers: [AppService],
